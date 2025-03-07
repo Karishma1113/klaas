fd1bca37e84ae6c210cab24707f4340a4f8a8913
samiiray <sy12345@uw.edu>
2025-03-06 21:59:12 -0800

    Saving local changes before pull

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./Navbar";
 import Home from "./Home";
-import Survey from "./Survey";
-import Resources from "./Resources";
-import ModulesHome from "./ModulesHome";
+import Survey from "./survey1"; // Corrected path
+import Resources from "./Resources"; // Corrected path
+import ModulesHome from "./ModulesHome"; // Corrected path
 
 export default function App() {
   return (
@@ -13,11 +13,9 @@ export default function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/survey" element={<Survey />} />
+          <Route path="/survey" element={<Survey />} /> 
           <Route path="/resources" element={<Resources />} />
           <Route path="/modules" element={<ModulesHome />} />
-
-          {/* Redirect unknown routes to homepage */}
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </div>
